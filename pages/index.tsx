bbddea38f468c88773debf261357547be543bf93
SAbi Sin <me@sabisin.com>
2022-08-25 08:32:51 +0200

    add styled components, fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,8 @@ const Home: NextPage = () => {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Exo:wght@300&family=Open+Sans:wght@300&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300&family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
       </Head>
       <main className={styles.main}>
         <div className="center">
